ab24cb9bc9b92a6bd1fc15b31401b22c3460ce24
bigred316 <price.g@stu.janesville.k12.wi.us>
2024-10-16 09:07:56 -0500

    10/16

---
+++ LP 4-3/LP 4-3/Program.py
@@ -0,0 +1,23 @@
+﻿eggs = float(input("How many eggs would you like? "))
+price = 0
+cost = 0 
+if eggs > 0 and eggs < 48:
+    price = float(0.50 / 12)
+elif eggs >= 48 and eggs < 72:
+    price = float(0.45 / 12)
+elif eggs >= 72 and eggs < 132:
+    price = float(0.40 / 12)
+elif eggs >= 132:
+    price = float(0.35 / 12)
+else:
+    print("Invalid Amount")
+
+
+
+cost = price * eggs 
+round(cost , 2)
+print("Your eggs will cost $" + str(cost))
+input()
+
+
+
