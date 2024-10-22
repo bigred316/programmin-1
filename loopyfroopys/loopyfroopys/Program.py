d24a54c8891c6bb289e7e8957b4c134ee41cd350
bigred316 <price.g@stu.janesville.k12.wi.us>
2024-10-22 09:12:44 -0500

    10/22

---
+++ loopyfroopys/loopyfroopys/Program.py
@@ -0,0 +1,19 @@
+﻿colors = ["red" , "green" , "blue"]
+print(colors)
+print(colors[2])
+fruits = ["apple" , "banana", "orange" , "kiwi", "mango"]
+print("")
+#for-each loop
+for item in fruits:
+    print(item)
+print("")
+for index in range(len(fruits)):
+    print(fruits[index])
+print("")
+
+
+lastfruit = fruits[len(fruits)-1]
+# OR this only works in python
+lastfruit = fruits[-1]
+print(lastfruit)
+input()
