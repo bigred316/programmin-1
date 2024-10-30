90ddfcdb043182858f5d719e03ce2ccd651c354b
bigred316 <price.g@stu.janesville.k12.wi.us>
2024-10-30 09:39:29 -0500

    10/30

---
+++ slices/slices/Program.py
@@ -0,0 +1,14 @@
+﻿list1 = [1,2,3,4,5]
+front = list1[:3]
+back = list1[3:]
+print(front)
+print(back)
+
+print(front+back)
+hello = "Hello, world!"
+substr = hello[:5]
+print(hello)
+print(substr)
+print(hello[-6:-1]) #[start,stop,step]
+print(hello == hello[::-1]) #is it a palindrome
+input()
