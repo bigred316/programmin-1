849017e3e640ccbc9c4f81945055cb21909f0b6a
bigred316 <price.g@stu.janesville.k12.wi.us>
2024-11-06 08:47:53 -0600

    11/6

---
+++ STRINGSTUFF/STRINGSTUFF/Program.py
@@ -0,0 +1,12 @@
+﻿thing = "HELLO world HERE is SOME text"
+print(thing.lower())
+print(thing.upper())
+print(thing.capitalize())
+print(thing.title())
+print(thing.isdigit())
+print(thing.find("world"))
+print(thing.count("E"))
+print(thing[::-1]) #reverse string
+print(thing.split(" "))
+print("Hello" * 5)
+input()
