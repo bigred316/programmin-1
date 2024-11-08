04b261b745f32d248232dca259c644575a72e8cb
bigred316 <price.g@stu.janesville.k12.wi.us>
2024-11-08 09:27:40 -0600

    11/8

--- pg153salarycalc/pg153salarycalc/MainForm.py
+++ pg153salarycalc/pg153salarycalc/MainForm.py
@@ -12,6 +12,12 @@ class MainForm(Form):
         self._label1 = System.Windows.Forms.Label()
         self._label2 = System.Windows.Forms.Label()
         self._label3 = System.Windows.Forms.Label()
+        self._label4 = System.Windows.Forms.Label()
+        self._textBox1 = System.Windows.Forms.TextBox()
+        self._textBox2 = System.Windows.Forms.TextBox()
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
         self.SuspendLayout()
         # 
         # label1
@@ -44,15 +50,89 @@ class MainForm(Form):
         self._label3.TabIndex = 2
         self._label3.Text = "Pay per pay period"
         # 
+        # label4
+        # 
+        self._label4.BackColor = System.Drawing.Color.DarkCyan
+        self._label4.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label4.Location = System.Drawing.Point(119, 138)
+        self._label4.Name = "label4"
+        self._label4.Size = System.Drawing.Size(100, 49)
+        self._label4.TabIndex = 3
+        # 
+        # textBox1
+        # 
+        self._textBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._textBox1.Location = System.Drawing.Point(119, 82)
+        self._textBox1.Name = "textBox1"
+        self._textBox1.Size = System.Drawing.Size(100, 26)
+        self._textBox1.TabIndex = 4
+        # 
+        # textBox2
+        # 
+        self._textBox2.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._textBox2.Location = System.Drawing.Point(119, 13)
+        self._textBox2.Name = "textBox2"
+        self._textBox2.Size = System.Drawing.Size(100, 26)
+        self._textBox2.TabIndex = 5
+        # 
+        # button1
+        # 
+        self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 14.25, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.Location = System.Drawing.Point(235, 13)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(130, 51)
+        self._button1.TabIndex = 6
+        self._button1.Text = "Calculate"
+        self._button1.UseVisualStyleBackColor = True
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 14.25, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.Location = System.Drawing.Point(235, 70)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(130, 51)
+        self._button2.TabIndex = 7
+        self._button2.Text = "Clear"
+        self._button2.UseVisualStyleBackColor = True
+        self._button2.Click += self.Button2Click
+        # 
+        # button3
+        # 
+        self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 14.25, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button3.ForeColor = System.Drawing.Color.Red
+        self._button3.Location = System.Drawing.Point(235, 136)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(130, 51)
+        self._button3.TabIndex = 8
+        self._button3.Text = "Exit"
+        self._button3.UseVisualStyleBackColor = True
+        self._button3.Click += self.Button3Click
+        # 
         # MainForm
         # 
         self.BackColor = System.Drawing.SystemColors.ActiveCaption
         self.ClientSize = System.Drawing.Size(377, 218)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Controls.Add(self._textBox2)
+        self.Controls.Add(self._textBox1)
+        self.Controls.Add(self._label4)
         self.Controls.Add(self._label3)
         self.Controls.Add(self._label2)
         self.Controls.Add(self._label1)
         self.Name = "MainForm"
         self.Text = "pg153salarycalc"
-        self.Load += self.MainFormLoad
         self.ResumeLayout(False)
-print("yikes")
+        self.PerformLayout()
+
+
+    def Button2Click(self, sender, e):
+        self._label4.Text =""
+
+    def Button1Click(self, sender, e):
+        self._label4.Text = str(float(self._textBox2.Text) / float(self._textBox1.Text))
+
+    def Button3Click(self, sender, e):
+        Application.Exit()
