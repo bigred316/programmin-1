7ff80ffd1fe12841aded88c4c11f9059d2e45951
bigred316 <price.g@stu.janesville.k12.wi.us>
2024-11-14 09:38:42 -0600

    11/14

--- pg240comm/pg240comm/MainForm.py
+++ pg240comm/pg240comm/MainForm.py
@@ -14,12 +14,27 @@ class MainForm(Form):
         self._textBox1 = System.Windows.Forms.TextBox()
         self._label2 = System.Windows.Forms.Label()
         self._textBox2 = System.Windows.Forms.TextBox()
+        self._label3 = System.Windows.Forms.Label()
+        self._label4 = System.Windows.Forms.Label()
+        self._label5 = System.Windows.Forms.Label()
+        self._label6 = System.Windows.Forms.Label()
+        self._label7 = System.Windows.Forms.Label()
+        self._label8 = System.Windows.Forms.Label()
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
         self._groupBox1.SuspendLayout()
         self.SuspendLayout()
         # 
         # groupBox1
         # 
         self._groupBox1.BackColor = System.Drawing.Color.FromArgb(128, 128, 255)
+        self._groupBox1.Controls.Add(self._label8)
+        self._groupBox1.Controls.Add(self._label7)
+        self._groupBox1.Controls.Add(self._label6)
+        self._groupBox1.Controls.Add(self._label5)
+        self._groupBox1.Controls.Add(self._label4)
+        self._groupBox1.Controls.Add(self._label3)
         self._groupBox1.Controls.Add(self._textBox2)
         self._groupBox1.Controls.Add(self._label2)
         self._groupBox1.Controls.Add(self._textBox1)
@@ -67,9 +82,96 @@ class MainForm(Form):
         self._textBox2.Size = System.Drawing.Size(100, 34)
         self._textBox2.TabIndex = 3
         # 
+        # label3
+        # 
+        self._label3.Location = System.Drawing.Point(41, 147)
+        self._label3.Name = "label3"
+        self._label3.Size = System.Drawing.Size(230, 35)
+        self._label3.TabIndex = 4
+        self._label3.Text = "Commision Rate:"
+        # 
+        # label4
+        # 
+        self._label4.Location = System.Drawing.Point(41, 195)
+        self._label4.Name = "label4"
+        self._label4.Size = System.Drawing.Size(230, 35)
+        self._label4.TabIndex = 5
+        self._label4.Text = "Commision:"
+        # 
+        # label5
+        # 
+        self._label5.Location = System.Drawing.Point(41, 245)
+        self._label5.Name = "label5"
+        self._label5.Size = System.Drawing.Size(230, 35)
+        self._label5.TabIndex = 6
+        self._label5.Text = "Net pay:"
+        # 
+        # label6
+        # 
+        self._label6.BackColor = System.Drawing.Color.FromArgb(192, 255, 255)
+        self._label6.ForeColor = System.Drawing.Color.Black
+        self._label6.Location = System.Drawing.Point(255, 147)
+        self._label6.Name = "label6"
+        self._label6.Size = System.Drawing.Size(100, 35)
+        self._label6.TabIndex = 7
+        # 
+        # label7
+        # 
+        self._label7.BackColor = System.Drawing.Color.FromArgb(192, 255, 255)
+        self._label7.ForeColor = System.Drawing.Color.Black
+        self._label7.Location = System.Drawing.Point(171, 195)
+        self._label7.Name = "label7"
+        self._label7.Size = System.Drawing.Size(119, 35)
+        self._label7.TabIndex = 8
+        # 
+        # label8
+        # 
+        self._label8.BackColor = System.Drawing.Color.FromArgb(192, 255, 255)
+        self._label8.ForeColor = System.Drawing.Color.Black
+        self._label8.Location = System.Drawing.Point(135, 245)
+        self._label8.Name = "label8"
+        self._label8.Size = System.Drawing.Size(136, 35)
+        self._label8.TabIndex = 9
+        # 
+        # button1
+        # 
+        self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 14.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.Location = System.Drawing.Point(12, 382)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(175, 84)
+        self._button1.TabIndex = 2
+        self._button1.Text = "Calculate"
+        self._button1.UseVisualStyleBackColor = True
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 14.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.Location = System.Drawing.Point(183, 382)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(175, 84)
+        self._button2.TabIndex = 3
+        self._button2.Text = "Clear"
+        self._button2.UseVisualStyleBackColor = True
+        self._button2.Click += self.Button2Click
+        # 
+        # button3
+        # 
+        self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 14.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button3.Location = System.Drawing.Point(359, 382)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(175, 84)
+        self._button3.TabIndex = 4
+        self._button3.Text = "Exit"
+        self._button3.UseVisualStyleBackColor = True
+        self._button3.Click += self.Button3Click
+        # 
         # MainForm
         # 
         self.ClientSize = System.Drawing.Size(546, 478)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
         self.Controls.Add(self._groupBox1)
         self.Name = "MainForm"
         self.Text = "pg240comm"
@@ -77,3 +179,33 @@ class MainForm(Form):
         self._groupBox1.PerformLayout()
         self.ResumeLayout(False)
 
+
+    def Button1Click(self, sender, e):
+        sales=float(self._textBox1.Text)
+        advance = float(self._textBox2.Text)
+        rate = 0
+        if sales < 10000:
+            rate = 0.05
+        elif sales in range(10000, 15000):
+            rate = 0.10
+        elif sales in range(15000, 18000):
+            rate = 0.12
+        elif sales in range(18000, 22000):
+            rate = 0.14
+        elif sales >= 22000:
+            rate = 0.16
+        commision = sales * rate
+        pay = commision - advance
+        self._label8.Text=str(pay)
+        self._label7.Text = str(commision)
+        self._label6.Text = str(rate*100) + "%"
+        
+            
+
+    def Button2Click(self, sender, e):
+        self._label8.Text=""
+        self._label7.Text = ""
+        self._label6.Text = ""
+
+    def Button3Click(self, sender, e):
+        Application.Exit()
---
+++ pg247radio/pg247radio/MainForm.py
@@ -0,0 +1,198 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._groupBox1 = System.Windows.Forms.GroupBox()
+        self._groupBox2 = System.Windows.Forms.GroupBox()
+        self._checkBox1 = System.Windows.Forms.CheckBox()
+        self._checkBox2 = System.Windows.Forms.CheckBox()
+        self._checkBox3 = System.Windows.Forms.CheckBox()
+        self._radioButton1 = System.Windows.Forms.RadioButton()
+        self._radioButton2 = System.Windows.Forms.RadioButton()
+        self._radioButton3 = System.Windows.Forms.RadioButton()
+        self._label1 = System.Windows.Forms.Label()
+        self._button1 = System.Windows.Forms.Button()
+        self._groupBox1.SuspendLayout()
+        self._groupBox2.SuspendLayout()
+        self.SuspendLayout()
+        # 
+        # groupBox1
+        # 
+        self._groupBox1.BackColor = System.Drawing.Color.FromArgb(255, 192, 192)
+        self._groupBox1.Controls.Add(self._radioButton3)
+        self._groupBox1.Controls.Add(self._radioButton2)
+        self._groupBox1.Controls.Add(self._radioButton1)
+        self._groupBox1.Font = System.Drawing.Font("Franklin Gothic Demi", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._groupBox1.Location = System.Drawing.Point(12, 13)
+        self._groupBox1.Name = "groupBox1"
+        self._groupBox1.Size = System.Drawing.Size(282, 311)
+        self._groupBox1.TabIndex = 1
+        self._groupBox1.TabStop = False
+        self._groupBox1.Text = "Radio Buttons"
+        self._groupBox1.Enter += self.GroupBox1Enter
+        # 
+        # groupBox2
+        # 
+        self._groupBox2.BackColor = System.Drawing.Color.FromArgb(255, 224, 192)
+        self._groupBox2.Controls.Add(self._checkBox3)
+        self._groupBox2.Controls.Add(self._checkBox2)
+        self._groupBox2.Controls.Add(self._checkBox1)
+        self._groupBox2.Font = System.Drawing.Font("Rockwell", 15.75, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._groupBox2.Location = System.Drawing.Point(300, 13)
+        self._groupBox2.Name = "groupBox2"
+        self._groupBox2.Size = System.Drawing.Size(304, 311)
+        self._groupBox2.TabIndex = 1
+        self._groupBox2.TabStop = False
+        self._groupBox2.Text = "Check Boxes"
+        # 
+        # checkBox1
+        # 
+        self._checkBox1.Location = System.Drawing.Point(19, 58)
+        self._checkBox1.Name = "checkBox1"
+        self._checkBox1.Size = System.Drawing.Size(240, 24)
+        self._checkBox1.TabIndex = 0
+        self._checkBox1.Text = "Choice 4"
+        self._checkBox1.UseVisualStyleBackColor = True
+        self._checkBox1.CheckedChanged += self.CheckBox1CheckedChanged
+        # 
+        # checkBox2
+        # 
+        self._checkBox2.Location = System.Drawing.Point(19, 131)
+        self._checkBox2.Name = "checkBox2"
+        self._checkBox2.Size = System.Drawing.Size(240, 24)
+        self._checkBox2.TabIndex = 1
+        self._checkBox2.Text = "Choice 5"
+        self._checkBox2.UseVisualStyleBackColor = True
+        self._checkBox2.CheckedChanged += self.CheckBox2CheckedChanged
+        # 
+        # checkBox3
+        # 
+        self._checkBox3.Location = System.Drawing.Point(19, 205)
+        self._checkBox3.Name = "checkBox3"
+        self._checkBox3.Size = System.Drawing.Size(240, 24)
+        self._checkBox3.TabIndex = 2
+        self._checkBox3.Text = "Choice 6"
+        self._checkBox3.UseVisualStyleBackColor = True
+        self._checkBox3.CheckedChanged += self.CheckBox3CheckedChanged
+        # 
+        # radioButton1
+        # 
+        self._radioButton1.Location = System.Drawing.Point(6, 58)
+        self._radioButton1.Name = "radioButton1"
+        self._radioButton1.Size = System.Drawing.Size(181, 24)
+        self._radioButton1.TabIndex = 0
+        self._radioButton1.TabStop = True
+        self._radioButton1.Text = "Choice 1"
+        self._radioButton1.UseVisualStyleBackColor = True
+        self._radioButton1.CheckedChanged += self.RadioButton1CheckedChanged
+        # 
+        # radioButton2
+        # 
+        self._radioButton2.Location = System.Drawing.Point(6, 131)
+        self._radioButton2.Name = "radioButton2"
+        self._radioButton2.Size = System.Drawing.Size(181, 24)
+        self._radioButton2.TabIndex = 1
+        self._radioButton2.TabStop = True
+        self._radioButton2.Text = "Choice 2"
+        self._radioButton2.UseVisualStyleBackColor = True
+        self._radioButton2.CheckedChanged += self.RadioButton2CheckedChanged
+        # 
+        # radioButton3
+        # 
+        self._radioButton3.Location = System.Drawing.Point(6, 205)
+        self._radioButton3.Name = "radioButton3"
+        self._radioButton3.Size = System.Drawing.Size(181, 24)
+        self._radioButton3.TabIndex = 2
+        self._radioButton3.TabStop = True
+        self._radioButton3.Text = "Choice 3"
+        self._radioButton3.UseVisualStyleBackColor = True
+        self._radioButton3.CheckedChanged += self.RadioButton3CheckedChanged
+        # 
+        # label1
+        # 
+        self._label1.BackColor = System.Drawing.Color.FromArgb(255, 255, 192)
+        self._label1.Font = System.Drawing.Font("Modern No. 20", 15.749999, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label1.Location = System.Drawing.Point(611, 13)
+        self._label1.Name = "label1"
+        self._label1.Size = System.Drawing.Size(240, 311)
+        self._label1.TabIndex = 2
+        self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        # 
+        # button1
+        # 
+        self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.ForeColor = System.Drawing.Color.Red
+        self._button1.Location = System.Drawing.Point(858, 13)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(45, 311)
+        self._button1.TabIndex = 3
+        self._button1.Text = "EXIT"
+        self._button1.UseVisualStyleBackColor = True
+        self._button1.Click += self.Button1Click
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.Color.Blue
+        self.ClientSize = System.Drawing.Size(912, 330)
+        self.Controls.Add(self._button1)
+        self.Controls.Add(self._label1)
+        self.Controls.Add(self._groupBox2)
+        self.Controls.Add(self._groupBox1)
+        self.Name = "MainForm"
+        self.Text = "pg247radio"
+        self._groupBox1.ResumeLayout(False)
+        self._groupBox2.ResumeLayout(False)
+        self.ResumeLayout(False)
+
+
+    def CheckBox2CheckedChanged(self, sender, e):
+        self.msg_str()
+    
+    def msg_str(self):
+        message=""
+        if self._radioButton1.Checked:
+            message = "You chose Choice 1"
+        elif self._radioButton2.Checked:
+            message = "You chose Choice 2"
+        elif self._radioButton3.Checked:
+            message = "You chose Choice 3"
+        
+        if self._checkBox1.Checked:
+            message += " and Choice 4"
+        if self._checkBox2.Checked:
+            message += " and Choice 5"
+        if self._checkBox3.Checked:
+            message += " and Choice 6"
+        message+="."
+        self._label1.Text = message
+        
+        
+        
+
+    def RadioButton1CheckedChanged(self, sender, e):
+        self.msg_str()
+
+    def Button1Click(self, sender, e):
+        Application.Exit()
+
+    def RadioButton2CheckedChanged(self, sender, e):
+        self.msg_str()
+
+    def GroupBox1Enter(self, sender, e):
+        pass
+
+    def RadioButton3CheckedChanged(self, sender, e):
+        self.msg_str()
+
+    def CheckBox1CheckedChanged(self, sender, e):
+        self.msg_str()
+
+    def CheckBox3CheckedChanged(self, sender, e):
+        self.msg_str()
