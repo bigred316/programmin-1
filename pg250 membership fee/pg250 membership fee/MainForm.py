cb5481e1f1f12af30526d3854d27aef58f5371f7
bigred316 <price.g@stu.janesville.k12.wi.us>
2024-11-26 09:00:39 -0600

    11/26

---
+++ pg250 membership fee/pg250 membership fee/MainForm.py
@@ -0,0 +1,295 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._groupBox1 = System.Windows.Forms.GroupBox()
+        self._radioButton1 = System.Windows.Forms.RadioButton()
+        self._radioButton2 = System.Windows.Forms.RadioButton()
+        self._radioButton3 = System.Windows.Forms.RadioButton()
+        self._radioButton4 = System.Windows.Forms.RadioButton()
+        self._groupBox2 = System.Windows.Forms.GroupBox()
+        self._label1 = System.Windows.Forms.Label()
+        self._textBox1 = System.Windows.Forms.TextBox()
+        self._groupBox3 = System.Windows.Forms.GroupBox()
+        self._checkBox1 = System.Windows.Forms.CheckBox()
+        self._checkBox2 = System.Windows.Forms.CheckBox()
+        self._checkBox3 = System.Windows.Forms.CheckBox()
+        self._groupBox4 = System.Windows.Forms.GroupBox()
+        self._label2 = System.Windows.Forms.Label()
+        self._label3 = System.Windows.Forms.Label()
+        self._label4 = System.Windows.Forms.Label()
+        self._label5 = System.Windows.Forms.Label()
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._groupBox1.SuspendLayout()
+        self._groupBox2.SuspendLayout()
+        self._groupBox3.SuspendLayout()
+        self._groupBox4.SuspendLayout()
+        self.SuspendLayout()
+        # 
+        # groupBox1
+        # 
+        self._groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        self._groupBox1.Controls.Add(self._radioButton4)
+        self._groupBox1.Controls.Add(self._radioButton3)
+        self._groupBox1.Controls.Add(self._radioButton2)
+        self._groupBox1.Controls.Add(self._radioButton1)
+        self._groupBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 14.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._groupBox1.Location = System.Drawing.Point(13, 13)
+        self._groupBox1.Name = "groupBox1"
+        self._groupBox1.Size = System.Drawing.Size(266, 222)
+        self._groupBox1.TabIndex = 0
+        self._groupBox1.TabStop = False
+        self._groupBox1.Text = "Membership type"
+        # 
+        # radioButton1
+        # 
+        self._radioButton1.Location = System.Drawing.Point(28, 41)
+        self._radioButton1.Name = "radioButton1"
+        self._radioButton1.Size = System.Drawing.Size(104, 24)
+        self._radioButton1.TabIndex = 0
+        self._radioButton1.TabStop = True
+        self._radioButton1.Text = "Adult"
+        self._radioButton1.UseVisualStyleBackColor = True
+        # 
+        # radioButton2
+        # 
+        self._radioButton2.Location = System.Drawing.Point(28, 81)
+        self._radioButton2.Name = "radioButton2"
+        self._radioButton2.Size = System.Drawing.Size(175, 31)
+        self._radioButton2.TabIndex = 1
+        self._radioButton2.TabStop = True
+        self._radioButton2.Text = "Child (Under 12)"
+        self._radioButton2.UseVisualStyleBackColor = True
+        # 
+        # radioButton3
+        # 
+        self._radioButton3.Location = System.Drawing.Point(28, 118)
+        self._radioButton3.Name = "radioButton3"
+        self._radioButton3.Size = System.Drawing.Size(104, 24)
+        self._radioButton3.TabIndex = 2
+        self._radioButton3.TabStop = True
+        self._radioButton3.Text = "Student"
+        self._radioButton3.UseVisualStyleBackColor = True
+        # 
+        # radioButton4
+        # 
+        self._radioButton4.Location = System.Drawing.Point(28, 157)
+        self._radioButton4.Name = "radioButton4"
+        self._radioButton4.Size = System.Drawing.Size(193, 29)
+        self._radioButton4.TabIndex = 3
+        self._radioButton4.TabStop = True
+        self._radioButton4.Text = "Senior Citizen (65+)"
+        self._radioButton4.UseVisualStyleBackColor = True
+        # 
+        # groupBox2
+        # 
+        self._groupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption
+        self._groupBox2.Controls.Add(self._textBox1)
+        self._groupBox2.Controls.Add(self._label1)
+        self._groupBox2.Font = System.Drawing.Font("Microsoft Sans Serif", 14.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._groupBox2.Location = System.Drawing.Point(12, 259)
+        self._groupBox2.Name = "groupBox2"
+        self._groupBox2.Size = System.Drawing.Size(266, 222)
+        self._groupBox2.TabIndex = 4
+        self._groupBox2.TabStop = False
+        self._groupBox2.Text = "Membership length"
+        # 
+        # label1
+        # 
+        self._label1.Location = System.Drawing.Point(6, 67)
+        self._label1.Name = "label1"
+        self._label1.Size = System.Drawing.Size(254, 77)
+        self._label1.TabIndex = 0
+        self._label1.Text = """Input number of months you want your membership to last:
+"""
+        # 
+        # textBox1
+        # 
+        self._textBox1.Location = System.Drawing.Point(50, 115)
+        self._textBox1.Name = "textBox1"
+        self._textBox1.Size = System.Drawing.Size(172, 29)
+        self._textBox1.TabIndex = 1
+        # 
+        # groupBox3
+        # 
+        self._groupBox3.BackColor = System.Drawing.SystemColors.ActiveCaption
+        self._groupBox3.Controls.Add(self._checkBox3)
+        self._groupBox3.Controls.Add(self._checkBox2)
+        self._groupBox3.Controls.Add(self._checkBox1)
+        self._groupBox3.Font = System.Drawing.Font("Microsoft Sans Serif", 14.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._groupBox3.Location = System.Drawing.Point(314, 13)
+        self._groupBox3.Name = "groupBox3"
+        self._groupBox3.Size = System.Drawing.Size(266, 222)
+        self._groupBox3.TabIndex = 4
+        self._groupBox3.TabStop = False
+        self._groupBox3.Text = "Add-Ons"
+        # 
+        # checkBox1
+        # 
+        self._checkBox1.Location = System.Drawing.Point(16, 40)
+        self._checkBox1.Name = "checkBox1"
+        self._checkBox1.Size = System.Drawing.Size(124, 35)
+        self._checkBox1.TabIndex = 0
+        self._checkBox1.Text = "Yoga"
+        self._checkBox1.UseVisualStyleBackColor = True
+        # 
+        # checkBox2
+        # 
+        self._checkBox2.Location = System.Drawing.Point(16, 81)
+        self._checkBox2.Name = "checkBox2"
+        self._checkBox2.Size = System.Drawing.Size(124, 35)
+        self._checkBox2.TabIndex = 1
+        self._checkBox2.Text = "Karate"
+        self._checkBox2.UseVisualStyleBackColor = True
+        # 
+        # checkBox3
+        # 
+        self._checkBox3.Location = System.Drawing.Point(16, 122)
+        self._checkBox3.Name = "checkBox3"
+        self._checkBox3.Size = System.Drawing.Size(199, 35)
+        self._checkBox3.TabIndex = 2
+        self._checkBox3.Text = "Personal Trainer"
+        self._checkBox3.UseVisualStyleBackColor = True
+        # 
+        # groupBox4
+        # 
+        self._groupBox4.BackColor = System.Drawing.SystemColors.ActiveCaption
+        self._groupBox4.Controls.Add(self._label5)
+        self._groupBox4.Controls.Add(self._label4)
+        self._groupBox4.Controls.Add(self._label3)
+        self._groupBox4.Controls.Add(self._label2)
+        self._groupBox4.Font = System.Drawing.Font("Microsoft Sans Serif", 14.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._groupBox4.Location = System.Drawing.Point(314, 259)
+        self._groupBox4.Name = "groupBox4"
+        self._groupBox4.Size = System.Drawing.Size(266, 222)
+        self._groupBox4.TabIndex = 5
+        self._groupBox4.TabStop = False
+        self._groupBox4.Text = "Totals"
+        # 
+        # label2
+        # 
+        self._label2.Location = System.Drawing.Point(6, 41)
+        self._label2.Name = "label2"
+        self._label2.Size = System.Drawing.Size(189, 23)
+        self._label2.TabIndex = 0
+        self._label2.Text = "Monthly Fee:"
+        # 
+        # label3
+        # 
+        self._label3.Location = System.Drawing.Point(6, 81)
+        self._label3.Name = "label3"
+        self._label3.Size = System.Drawing.Size(100, 23)
+        self._label3.TabIndex = 1
+        self._label3.Text = "Total:"
+        # 
+        # label4
+        # 
+        self._label4.BackColor = System.Drawing.SystemColors.ButtonFace
+        self._label4.Location = System.Drawing.Point(129, 41)
+        self._label4.Name = "label4"
+        self._label4.Size = System.Drawing.Size(117, 23)
+        self._label4.TabIndex = 2
+        # 
+        # label5
+        # 
+        self._label5.BackColor = System.Drawing.SystemColors.ButtonFace
+        self._label5.Location = System.Drawing.Point(60, 81)
+        self._label5.Name = "label5"
+        self._label5.Size = System.Drawing.Size(117, 23)
+        self._label5.TabIndex = 3
+        # 
+        # button1
+        # 
+        self._button1.Location = System.Drawing.Point(62, 488)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(203, 25)
+        self._button1.TabIndex = 6
+        self._button1.Text = "Calculate"
+        self._button1.UseVisualStyleBackColor = True
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.Location = System.Drawing.Point(320, 487)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(203, 25)
+        self._button2.TabIndex = 7
+        self._button2.Text = "Exit"
+        self._button2.UseVisualStyleBackColor = True
+        self._button2.Click += self.Button2Click
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.SystemColors.Highlight
+        self.ClientSize = System.Drawing.Size(605, 525)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Controls.Add(self._groupBox4)
+        self.Controls.Add(self._groupBox3)
+        self.Controls.Add(self._groupBox2)
+        self.Controls.Add(self._groupBox1)
+        self.Name = "MainForm"
+        self.Text = "pg250 membership fee"
+        self.Load += self.MainFormLoad
+        self._groupBox1.ResumeLayout(False)
+        self._groupBox2.ResumeLayout(False)
+        self._groupBox2.PerformLayout()
+        self._groupBox3.ResumeLayout(False)
+        self._groupBox4.ResumeLayout(False)
+        self.ResumeLayout(False)
+
+
+    def MainFormLoad(self, sender, e):
+        pass
+
+    def Button2Click(self, sender, e):
+        Application.Exit()
+
+    def Button1Click(self, sender, e):
+        basefee = 0.0
+        months = int(self._textBox1.Text)
+        
+        if months < 1 or months > 24:
+            MessageBox.Show("Please input a month value greater than 0 and less than 25")
+            return
+        
+        if self._radioButton1.Checked == True:
+            basefee=40
+        if self._radioButton2.Checked == True:
+            basefee=20    
+        if self._radioButton3.Checked == True:
+            basefee=25
+        if self._radioButton4.Checked == True:
+            basefee=30
+        if self._checkBox1.Checked == True:
+            basefee += 10
+        if self._checkBox2.Checked == True:
+            basefee += 30
+        if self._checkBox3.Checked == True:
+            basefee += 50
+        if months <= 3:
+            basefee -= basefee * 0 
+        if months in range(4, 7):
+            basefee -= basefee * 0.05
+        if months in range(7, 10):
+            basefee -= basefee * 0.08
+        if months >= 10:
+            basefee -= basefee * 0.1
+        self._label4.Text = str(basefee)
+        self._label5.Text = str(basefee* months)
+        
+            
+        
+            
+        
+
+
+        
--- slot machine/slot machine/MainForm.py
+++ slot machine/slot machine/MainForm.py
@@ -18,12 +18,12 @@ class MainForm(Form):
         self._pictureBox2 = System.Windows.Forms.PictureBox()
         self._pictureBox3 = System.Windows.Forms.PictureBox()
         self._pictureBox4 = System.Windows.Forms.PictureBox()
-        self._gamble = System.Windows.Forms.Button()
-        self._therightchoice = System.Windows.Forms.Button()
+        self._button1 = System.Windows.Forms.Button()
+        self._steal = System.Windows.Forms.Button()
         self._label1 = System.Windows.Forms.Label()
         self._textBox1 = System.Windows.Forms.TextBox()
         self._label2 = System.Windows.Forms.Label()
-        self._collegefund = System.Windows.Forms.Label()
+        self._label3 = System.Windows.Forms.Label()
         self._progressBar1 = System.Windows.Forms.ProgressBar()
         self._pictureBox5 = System.Windows.Forms.PictureBox()
         self._pictureBox6 = System.Windows.Forms.PictureBox()
@@ -49,27 +49,30 @@ class MainForm(Form):
         # pictureBox1
         # 
         self._pictureBox1.BackColor = System.Drawing.Color.White
-        self._pictureBox1.Location = System.Drawing.Point(62, 50)
+        self._pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        self._pictureBox1.Location = System.Drawing.Point(12, 12)
         self._pictureBox1.Name = "pictureBox1"
-        self._pictureBox1.Size = System.Drawing.Size(100, 209)
+        self._pictureBox1.Size = System.Drawing.Size(135, 265)
         self._pictureBox1.TabIndex = 0
         self._pictureBox1.TabStop = False
         # 
         # pictureBox2
         # 
         self._pictureBox2.BackColor = System.Drawing.Color.White
-        self._pictureBox2.Location = System.Drawing.Point(190, 50)
+        self._pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        self._pictureBox2.Location = System.Drawing.Point(169, 12)
         self._pictureBox2.Name = "pictureBox2"
-        self._pictureBox2.Size = System.Drawing.Size(100, 209)
+        self._pictureBox2.Size = System.Drawing.Size(145, 265)
         self._pictureBox2.TabIndex = 1
         self._pictureBox2.TabStop = False
         # 
         # pictureBox3
         # 
         self._pictureBox3.BackColor = System.Drawing.Color.White
-        self._pictureBox3.Location = System.Drawing.Point(325, 50)
+        self._pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        self._pictureBox3.Location = System.Drawing.Point(338, 12)
         self._pictureBox3.Name = "pictureBox3"
-        self._pictureBox3.Size = System.Drawing.Size(100, 209)
+        self._pictureBox3.Size = System.Drawing.Size(150, 265)
         self._pictureBox3.TabIndex = 2
         self._pictureBox3.TabStop = False
         # 
@@ -83,28 +86,29 @@ class MainForm(Form):
         self._pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         self._pictureBox4.TabIndex = 3
         self._pictureBox4.TabStop = False
+        self._pictureBox4.Visible = False
         # 
-        # gamble
+        # button1
         # 
-        self._gamble.BackgroundImage = resources.GetObject("gamble.BackgroundImage")
-        self._gamble.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        self._gamble.Location = System.Drawing.Point(515, 50)
-        self._gamble.Name = "gamble"
-        self._gamble.Size = System.Drawing.Size(221, 261)
-        self._gamble.TabIndex = 4
-        self._gamble.UseVisualStyleBackColor = True
-        self._gamble.Click += self.GambleClick
+        self._button1.BackgroundImage = resources.GetObject("button1.BackgroundImage")
+        self._button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        self._button1.Location = System.Drawing.Point(515, 50)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(221, 261)
+        self._button1.TabIndex = 3
+        self._button1.UseVisualStyleBackColor = True
+        self._button1.Click += self.Button1Click
         # 
-        # therightchoice
+        # steal
         # 
-        self._therightchoice.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._therightchoice.Location = System.Drawing.Point(554, 317)
-        self._therightchoice.Name = "therightchoice"
-        self._therightchoice.Size = System.Drawing.Size(134, 40)
-        self._therightchoice.TabIndex = 5
-        self._therightchoice.Text = "Pickpocket"
-        self._therightchoice.UseVisualStyleBackColor = True
-        self._therightchoice.Click += self.TherightchoiceClick
+        self._steal.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._steal.Location = System.Drawing.Point(554, 317)
+        self._steal.Name = "steal"
+        self._steal.Size = System.Drawing.Size(134, 40)
+        self._steal.TabIndex = 5
+        self._steal.Text = "Pickpocket"
+        self._steal.UseVisualStyleBackColor = True
+        self._steal.Click += self.StealClick
         # 
         # label1
         # 
@@ -113,7 +117,7 @@ class MainForm(Form):
         self._label1.Location = System.Drawing.Point(515, 378)
         self._label1.Name = "label1"
         self._label1.Size = System.Drawing.Size(100, 23)
-        self._label1.TabIndex = 6
+        self._label1.TabIndex = 4
         self._label1.Text = "Bet:"
         # 
         # textBox1
@@ -134,15 +138,15 @@ class MainForm(Form):
         self._label2.TabIndex = 9
         self._label2.Text = "Cash:"
         # 
-        # collegefund
+        # label3
         # 
-        self._collegefund.BackColor = System.Drawing.Color.FromArgb(192, 255, 192)
-        self._collegefund.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._collegefund.Location = System.Drawing.Point(567, 421)
-        self._collegefund.Name = "collegefund"
-        self._collegefund.Size = System.Drawing.Size(100, 23)
-        self._collegefund.TabIndex = 10
-        self._collegefund.Text = "100"
+        self._label3.BackColor = System.Drawing.Color.FromArgb(192, 255, 192)
+        self._label3.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label3.Location = System.Drawing.Point(567, 421)
+        self._label3.Name = "label3"
+        self._label3.Size = System.Drawing.Size(100, 23)
+        self._label3.TabIndex = 10
+        self._label3.Text = "100"
         # 
         # progressBar1
         # 
@@ -243,6 +247,10 @@ class MainForm(Form):
         self._pictureBox11.TabStop = False
         self._pictureBox11.Visible = False
         # 
+        # timer1
+        # 
+        self._timer1.Tick += self.Timer1Tick
+        # 
         # MainForm
         # 
         self.BackColor = System.Drawing.Color.FromArgb(255, 255, 192)
@@ -255,12 +263,12 @@ class MainForm(Form):
         self.Controls.Add(self._pictureBox6)
         self.Controls.Add(self._pictureBox5)
         self.Controls.Add(self._progressBar1)
-        self.Controls.Add(self._collegefund)
+        self.Controls.Add(self._label3)
         self.Controls.Add(self._label2)
         self.Controls.Add(self._textBox1)
         self.Controls.Add(self._label1)
-        self.Controls.Add(self._therightchoice)
-        self.Controls.Add(self._gamble)
+        self.Controls.Add(self._steal)
+        self.Controls.Add(self._button1)
         self.Controls.Add(self._pictureBox4)
         self.Controls.Add(self._pictureBox3)
         self.Controls.Add(self._pictureBox2)
@@ -287,27 +295,27 @@ class MainForm(Form):
     def CollegefundClick(self, sender, e):
         pass
 
-    def TherightchoiceClick(self, sender, e):
+    def StealClick(self, sender, e):
         rnd = System.Random()
         money= rnd.Next(1, 51)
         if money > 25:
-            MessageBox.Show("You failed to steal money! yay!")
+            MessageBox.Show("You failed to steal money! Yay!")
         else:
-            funds = float(self.collegefund.Text)
-            self.collegefund.Text = str(round(funds + money, 2))
+            funds = float(self._label3.Text)
+            self._label3.Text = str(round(funds + money, 2))
                 
         
         pass
     
 
-    def GambleClick(self, sender, e):
-        im1 = self._pictureBox5.BackroundImage
-        im2 = self._pictureBox6.BackroundImage
-        im3 = self._pictureBox7.BackroundImage
-        im4 = self._pictureBox8.BackroundImage
-        im5 = self._pictureBox9.BackroundImage
-        levoff = self._pictureBox11.BackroundImage
-        levon = self._pictureBox10.BackroundImage
+    def Button1Click(self, sender, e):
+        im1 = self._pictureBox5.BackgroundImage
+        im2 = self._pictureBox6.BackgroundImage
+        im3 = self._pictureBox7.BackgroundImage
+        im4 = self._pictureBox8.BackgroundImage
+        im5 = self._pictureBox9.BackgroundImage
+        levoff = self._pictureBox11.BackgroundImage
+        levon = self._pictureBox10.BackgroundImage
         rnd = System.Random()
         num1 = 0
         num2 = 0
@@ -316,13 +324,88 @@ class MainForm(Form):
         if self._textBox1.Text == "":
             MessageBox.Show("You cant play without a bet, silly")
             return
-        money = float(slef._collegefund.Text)
-        bet = float(self._textbox1.Text)
+        money = float(self._label3.Text)
+        bet = float(self._textBox1.Text)
         newmoney = money - bet
-        if money == 0:
-            MessageBox.Show("Youre outta money kid. Go steal some or something, but sure as hell dont stop playing.")
+        if money <= 0:
+            MessageBox.Show("You don't got any more cash")
         elif bet < 1:
-            MessageBox.Show("C'mon kid, ya broke? At least bet a dollar!")
+            MessageBox.Show("At least bet a dollar!")
         elif bet > money:
-            MessageBox.Show("Youre supposed to bluff your cards, not whether or not you've got the money.")
+            MessageBox.Show("Dont bet more than you have.")
+        else:
+            self._button1.BackgroundImage = levon
+            self._button1.Enabled = False
+            self._pictureBox4.Visible=True
+            self._timer1.Enabled= True
+            self._progressBar1.Value = 0
+            self._label3.Text = str(round(newmoney , 2))
+            
+            num1 =self.num1
+            num2 =self.num2
+            num3 =self.num3
+            
+            if num1 == 1 and num2 ==1 and num3 == 1:
+                newmoney += bet*2
+            if num2 == num1 == num3 == 2:
+                newmoney += bet*2.2
+            if num2 == num1 == num3 == 3:
+                newmoney += bet*2.5
+            if num2 == num1 == num3 == 4:
+                newmoney += bet*3
+            if num2 == num1 == num3 == 5:
+                newmoney += bet*3.5
+            if num2 == num1 == num3 == 6:
+                newmoney += bet*4
+            if num2 == num1 == num3 == 7:
+                newmoney += bet*100
+            if num2 == num1 == num3 == 8:
+                newmoney += bet*6
+            if num2 == num1 == num3 == 9:
+                newmoney += bet*10
+            if num2 == num1 == num3 == 0:
+                newmoney += bet*0
+                
+            self.num1 = 0
+            self.num2 = 0
+            self.num3 = 0
+            
+
+            
+            self._label3.Text = str(round(newmoney, 2))
+            if newmoney <= 0:
+                MessageBox.Show("You ran out of money.")
         pass
+
+    def Timer1Tick(self, sender, e):
+        im1 = self._pictureBox5.BackgroundImage
+        im2 = self._pictureBox6.BackgroundImage
+        im3 = self._pictureBox7.BackgroundImage
+        im4 = self._pictureBox8.BackgroundImage
+        im5 = self._pictureBox9.BackgroundImage
+        levoff = self._pictureBox11.BackgroundImage
+        levon = self._pictureBox10.BackgroundImage
+        rnd = System.Random()
+        num1 = 0
+        num2 = 0
+        num3 = 0
+        
+        pb1 = self._pictureBox1
+        pb2 = self._pictureBox2
+        pb3 = self._pictureBox3
+        images = [im1,im2,im3,im4,im5]
+        for lcv in range(0, 1000):
+            self.num1 = rnd.Next(1, 6)
+            self.num2 = rnd.Next(1, 6)
+            self.num3 = rnd.Next(1, 6)
+            
+            self._pictureBox1.BackgroundImage = images[self.num1-1]
+            self._pictureBox2.BackgroundImage = images[self.num2-1]
+            self._pictureBox3.BackgroundImage = images[self.num3-1]
+            
+            self._progressBar1.Increment(1)
+            if self._progressBar1.Value==self._progressBar1.Maximum:
+                self._timer1.Enabled=False
+                self._pictureBox4.Visible=False
+                self._button1.BackgroundImage = levoff
+                self._button1.Enabled = True
